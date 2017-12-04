2119d6d5bec674f73c7be8653154c9ff1d4d027a
Paul26 <paulsheets26@gmail.com>
2017-12-03 18:03:38 -0700

    Convert string to double
    Converted column4 from a string to a double using stod().

--- check.cpp
+++ check.cpp
@@ -1,12 +1,73 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip>  // Need?
+#include <iomanip>  
 #include <string>
 
 using namespace std;
 
-int main()
+int main()  // (int argc, char* argv[])
 {
+	string inputFile;
+	
+	cout << "Please enter a file name to be parsed: " << endl;
+	cin >> inputFile;
+
+	ifstream in(inputFile);
+
+	if (!in.good())
+	{
+		cerr << "Unable to open file " << inputFile << endl;
+		exit(1);
+	}
+
+	//string line;
+
+	//while (getline(in, line))
+	//{
+	//	cout << line << endl;
+	//}
+
+	for (int i = 0; i < 80; i++)
+	{
+		cout << '-';
+	}
+
+	cout << endl;
+
+	cout.setf(ios::fixed);
+	cout.precision(2);
+
+
+	int amount;
+
+	while (!in.eof())
+	{
+		string column1;
+		getline(in, column1, ':');
+		cout <<  left << setw(15) << column1;
+
+		string column2;
+		getline(in, column2, ':');
+		cout << setw(15) << column2;
+
+		string column3;
+		getline(in, column3, ':');
+		cout << setw(15) << column3;
+
+		string column4;
+		string::size_type sz;
+		getline(cin, column4, ':');
+		double amount = stod(column4, &sz);
+		cout << setw(15) << amount << endl;
+	}
+
+
+
+	for (int i = 0; i < 80; i++)
+	{
+		cout << '-';
+	}
+	cout << endl;
 
 	system("pause");
 	return 0;
